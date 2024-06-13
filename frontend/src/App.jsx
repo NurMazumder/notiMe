ba5575a33a5ffe927ad6347219bddbe4adbf5536
Nur Mazumder <nmaz4828@gmail.com>
2024-06-12 22:57:46 -0400

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-
+import HomePage from "./pages/HomePage/HomePage";
 import "./App.css";
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-    </>
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          {/* Other routes can be added here */}
+        </Routes>
+      </>
+    </Router>
   );
 };
 
