ffae513a81d81a0c453fa30d9509b68159f8ec97
Nur Mazumder <nmaz4828@gmail.com>
2024-06-13 13:19:35 -0400

    navbar container

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,14 +4,20 @@ import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div id="header-container">
-      <Link to="/">
-        <div id="headerSmall"></div>
-      </Link>
-      <div id="header-menu">
-        <div className="header-menu-login">
-          <Link to="/login" className="btn-login">Login</Link>
-          <Link to="/signup" className="btn-signup">Sign Up</Link>
+    <div className="navbar-wrapper">
+      <div className="navbar-container">
+        <Link to="/">
+          <div id="headerSmall"></div>
+        </Link>
+        <div id="header-menu">
+          <div className="header-menu-login">
+            <Link to="/login" className="btn-login">
+              Login
+            </Link>
+            <Link to="/signup" className="btn-signup">
+              Sign Up
+            </Link>
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -4,24 +4,33 @@ import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <section className="home">
-      <div className="dark-overlay">
-        <div className="home-inner">
-          <h1 className="x-large">NotiMe</h1>
-          <p className="lead">
-          NotiMe allows for avid readers of Manhwa, manga, and web novels. It addresses the common challenge faced by foreign readers who struggle to keep track of the latest releases, often scattered across various third-party sites. NotiMe provides a centralized platform where users can stay updated on the newest chapters, bookmark their favorite titles, and easily manage their reading lists. With NotiMe, you can ensure you never miss an update on your favorite stories again.
-          </p>
-          <div className="buttons">
-            <Link to="/login" className="btn-login">
-              Login
-            </Link>
-            <Link to="/signup" className="btn-signup">
-              Sign Up
-            </Link>
+    <div className="navbar-container">
+      <section className="home">
+        <div className="dark-overlay">
+          <div className="home-inner">
+            <h1 className="x-large">NotiMe</h1>
+            <p className="lead">
+              NotiMe allows for avid readers of Manhwa, manga, and web novels.
+              It addresses the common challenge faced by foreign readers who
+              struggle to keep track of the latest releases, often scattered
+              across various third-party sites. NotiMe provides a centralized
+              platform where users can stay updated on the newest chapters,
+              bookmark their favorite titles, and easily manage their reading
+              lists. With NotiMe, you can ensure you never miss an update on
+              your favorite stories again.
+            </p>
+            <div className="buttons">
+              <Link to="/login" className="btn-login">
+                Login
+              </Link>
+              <Link to="/signup" className="btn-signup">
+                Sign Up
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
