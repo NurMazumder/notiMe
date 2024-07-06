b6b6e084864c0a424a929ee0998de079feaf3170
NurMazumder <nmaz4828@gmail.com>
2024-07-06 11:33:57 -0400

    scrapper agents

--- scrap/AsuraScrap.py
+++ scrap/AsuraScrap.py
@@ -5,7 +5,12 @@ import sys
 
 def scrape(url):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Connection': 'keep-alive',
+        'DNT': '1'
     }
 
     try:
